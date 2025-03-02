2543f8b314453742847a26abc4fa74a13b52ee04
Jacob DIAKITE <152518722+jdiakite007@users.noreply.github.com>
2025-03-02 15:41:57 +0100

    Update 03. with variables.tf

--- Hello Word/03. with variables.tf
+++ Hello Word/03. with variables.tf
@@ -1,1 +1,19 @@
+#Declaration de local variables
+locals {
+  weight = 67.88
+  age = 35
+  name = "John"
+}
 
+#using local variables in output
+output "using_string_var" {
+    value = local.name
+}
+
+output "numbered_output" {
+    value = local.age
+}
+
+output "decimal_output" {
+    value = local.weight
+}
