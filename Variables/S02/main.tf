20a1cccb0cae76e98f7b06185e9d68c47c32fae6
Jacob DIAKITE <152518722+jdiakite007@users.noreply.github.com>
2025-03-02 18:43:43 +0100

    Update main.tf

--- Variables/S02/main.tf
+++ Variables/S02/main.tf
@@ -1,7 +1,7 @@
 ```
 resource "aws_instance" "my_server" {
   ami           = "ami-0730f1cd4a7583808" //Ubuntu AMI
-  instance_type = "t2.micro"
+  instance_type = "var.instance_type"
   tags = {
     Name = "My EC2 Instance"
   }
