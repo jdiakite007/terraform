8fa0f442f7267d9c3918beab60a792a61f46bc37
Jacob DIAKITE <152518722+jdiakite007@users.noreply.github.com>
2025-03-02 16:17:39 +0100

    Update main.tf

--- Variables/main.tf
+++ Variables/main.tf
@@ -1,3 +1,4 @@
+`` `
 resource "aws_instance" "my_first_vm" {
   ami           = "ami-0730f1cd4a7583808" //Ubuntu AMI
   instance_type = "t2.micro"
@@ -5,3 +6,4 @@ resource "aws_instance" "my_first_vm" {
     Name = "My EC2 Instance"
 
   }
+`` `
